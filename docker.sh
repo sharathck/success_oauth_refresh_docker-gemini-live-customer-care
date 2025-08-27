6e91dab3126b5b4d0cb81f2dae331f9cb033f288
sharathck <sharathck@users.noreply.github.com>
2025-08-27 10:05:10 -0500

    Refactor docker.sh to enhance cleanup process by adding container and image removal steps before loading environment variables.

--- docker.sh
+++ docker.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+echo "Killing all Docker containers..."
+
+# Kill all running containers
+docker ps -q | xargs -r docker kill
+
+# Remove all containers (running and stopped)
+docker ps -aq | xargs -r docker rm
+
+echo "All Docker containers have been killed and removed."
+
+# Remove all images
+echo "Removing all Docker images..."
+docker images -q | xargs -r docker rmi -f
+
+# Prune system to clean up everything
+echo "Pruning Docker system..."
+docker system prune -af --volumes
+
+echo "Done! All Docker containers, images, and volumes have been removed."
 # Load environment variables from .env file
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
